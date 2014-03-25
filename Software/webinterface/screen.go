0862fe24bc70e201bd61e92efd02044abf152225
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-25 20:09:31 +0100

    webinterface: screen grid

--- Software/webinterface/screen.go
+++ Software/webinterface/screen.go
@@ -8,27 +8,34 @@ import(
 )
 
 var(
-	screenW, screenH = 512, 512
+	screenW, screenH = 512, 256
+	gridDiv = 32
 )
 
 func screenHandler(w http.ResponseWriter, r*http.Request){
 	w.Header().Set("Content-Type", "image/svg+xml")
     w.Header().Set("Cache-control", "No-Cache")
 
-	const S = 1
-	W,H := 512, 512
-
 	canvas := svg.New(w)
-	canvas.Start(S*W, S*H)
+	canvas.Start(screenW, screenH)
 
+	// Grid
+	for i := 0; i < screenW; i+= gridDiv{
+		canvas.Line(i, 0, i, screenH, "stroke:grey;")
+	}
+	for i := 0; i < screenH; i+= gridDiv{
+		canvas.Line(0, i, screenW, i, "stroke:grey;")
+	}
+	canvas.Rect(0, 0, screenW, screenH, "stroke:black; fill:none; stroke-width:3")
+
+	// Data
 	x := make([]int, BUFSIZE)
 	y := make([]int, BUFSIZE)
 	for i := range buf{
 		x[i] = i
-		y[i] = int(buf[i])
+		y[i] = screenH - int(buf[i])
 	}
-
-	canvas.Polyline(x, y, "stroke:red")
+	canvas.Polyline(x, y, "stroke:blue; fill:none; stroke-width:1")
 
 	canvas.End()
 }
