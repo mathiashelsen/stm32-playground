722efddadb645c2f0552bd1405a4b5ff7c076c5f
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-27 21:36:03 +0100

    add usart.c in preparation of serial communication

---
+++ Project/softScope/usart.h
@@ -0,0 +1,10 @@
+#ifndef _USART_H_
+#define _USART_H_
+
+// Init USART1 with TX on pin PB6, RX on PB7
+void init_USART1(uint32_t baudrate);
+
+// Send N bytes of data. Spins while waiting for transfer.
+void USART_TX(USART_TypeDef* USARTx, uint8_t *data, uint16_t N);
+
+#endif
