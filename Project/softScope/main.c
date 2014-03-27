d8499a12f176bdde24e70480952da728841461a5
Mathias Helsen <Mathias.Helsen@UGent.be>
2014-03-27 11:11:09 +0100

    enabling pin a1 causes a freeze?!

--- Project/softScope/main.c
+++ Project/softScope/main.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include <stdint.h>
 #include <math.h>
+#include <stdlib.h>
 
-volatile uint16_t samples[2048];
+volatile uint16_t *samples;
 
 #define TIM_PERIOD 42000
 
@@ -17,6 +17,8 @@ void init_ADC(void);
 int main(void)
 {
     NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); 
+
+    samples = malloc(sizeof(uint16_t)*2048);
 
     init_clock(); // TIM2 running at 1MHz
     init_ADC();
@@ -62,12 +64,14 @@ void init_ADC(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 
     // The things that are shared between the three ADCs
+   
     ADC_CommonInitTypeDef common = {0, };
     common.ADC_Mode = ADC_Mode_Independent;
     common.ADC_Prescaler = ADC_Prescaler_Div2;
     ADC_CommonInit( &common );
-
+  
     // The things specific to ADC1
+ 
     ADC_InitTypeDef adc = {0, };
     adc.ADC_Resolution = ADC_Resolution_12b;
     adc.ADC_ScanConvMode = DISABLE; // Disable scanning multiple channels
@@ -77,6 +81,7 @@ void init_ADC(void)
     adc.ADC_DataAlign = ADC_DataAlign_Right;
     adc.ADC_NbrOfConversion = 1;
     ADC_Init( ADC1, &adc );
+
     
     // Configure the channel from which to sample
     ADC_RegularChannelConfig( ADC1, ADC_Channel_1, 0, ADC_SampleTime_3Cycles);
@@ -89,6 +94,7 @@ void init_ADC(void)
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
 
     // Configure the DMA stream for ADC -> memory
+    
     DMA_InitTypeDef DMAInit = {0, };
     DMAInit.DMA_Channel	    = DMA_Channel_0; // DMA channel 0 stream 0 is mapped to ADC1
     DMAInit.DMA_PeripheralBaseAddr  = (uint32_t) ADC1->DR;
@@ -104,29 +110,33 @@ void init_ADC(void)
     DMAInit.DMA_FIFOMode    = DMA_FIFOMode_Disable; // No FIFO, direct write will be sufficiently fast
     DMAInit.DMA_MemoryBurst = DMA_MemoryBurst_Single;
     DMAInit.DMA_PeripheralBurst	    = DMA_PeripheralBurst_Single;
-    
+   
     // Initialize the DMA
+    
     DMA_Init( DMA2_Stream0, &DMAInit );
     DMA_Cmd( DMA2_Stream0 , ENABLE );
-
+    
     // Now sampling from the input pin PA1
+    /*
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-    GPIO_InitTypeDef gpio = {0, };
-    gpio.GPIO_Pin = GPIO_Pin_1;
-    gpio.GPIO_Mode = GPIO_Mode_AN;
-    GPIO_Init(GPIOA, &gpio);
-
-
+    GPIO_InitTypeDef gpioInit = {0, };
+    gpioInit.GPIO_Pin = GPIO_Pin_1;
+    gpioInit.GPIO_Mode = GPIO_Mode_AN;
+    GPIO_Init(GPIOA, &gpioInit);
+    */
+   
     // Now configure an interrupt halfway and at the end of the transfer
+
     DMA_ITConfig( DMA2_Stream0, DMA_IT_TC | DMA_IT_HT, ENABLE);
     DMA_ClearITPendingBit(DMA2_Stream0, DMA_IT_TC | DMA_IT_HT);
-     
+    
     NVIC_InitTypeDef NVICInit = {0, };
     NVICInit.NVIC_IRQChannel = DMA2_Stream0_IRQn;
     NVICInit.NVIC_IRQChannelPreemptionPriority = 0;
     NVICInit.NVIC_IRQChannelSubPriority = 0;
     NVICInit.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVICInit);
+
 }
 
 void DMA2_Stream0_IRQHandler(void)
