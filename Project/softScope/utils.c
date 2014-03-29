1e877161de83ca1c746aa05907e6ca4342e11a42
Mathias Helsen <mathias.helsen@ugent.be>
2014-03-29 16:03:12 +0100

    faster copy routine than memcpy

--- Project/softScope/utils.c
+++ Project/softScope/utils.c
@@ -3,11 +3,13 @@
 void memcpy32(void *dest, void *src, uint32_t nBytes)
 {
     uint32_t N = nBytes >> 4;
+    uint32_t *d = (uint32_t *) dest;
+    uint32_t *s = (uint32_t *) src;
     do
     {
-	*dest++ = *src++;
-	*dest++ = *src++;
-	*dest++ = *src++;
-	*dest++ = *src++;
+	*d++ = *s++;
+	*d++ = *s++;
+	*d++ = *s++;
+	*d++ = *s++;
     }while(--N);
 }
