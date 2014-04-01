e7806c35aee165ca42e142e4b41aa592d74ad849
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-04-01 20:44:12 +0200

    leds.c now working

--- Project/softScope/leds.c
+++ Project/softScope/leds.c
@@ -2,15 +2,22 @@
 
 #include "leds.h"
 
-void init_LEDS(void) {
+void init_LEDs(void) {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	GPIO_InitTypeDef gpio = {0, };
-	gpio.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14;
+	gpio.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	gpio.GPIO_Mode = GPIO_Mode_OUT;
 	gpio.GPIO_OType = GPIO_OType_PP;
 	gpio.GPIO_Speed = GPIO_Speed_25MHz;
 	gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &gpio);
-	GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 );
+	GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15 );
 }
 
+void LEDOn(uint16_t led){
+	GPIO_SetBits(GPIOD, led);
+}
+
+void LEDOff(uint16_t led){
+	GPIO_ResetBits(GPIOD, led);
+}
--- Project/softScope/leds.h
+++ Project/softScope/leds.h
@@ -1,7 +1,17 @@
 #ifndef _LEDS_H_
 #define _LEDS_H_
 
+#include <stm32f4xx.h>
+
+#define LED1 GPIO_Pin_12  // green
+#define LED2 GPIO_Pin_13  // orange 
+#define LED3 GPIO_Pin_14  // red
+#define LED4 GPIO_Pin_15  // blue
+
 // Initialize GPIO for the onboard LEDS
-void init_LEDS(void);
+void init_LEDs();
+
+void LEDOn (uint16_t led);
+void LEDOff(uint16_t led);
 
 #endif
