9f536a41e3518762a9713b4e75b01847d2022414
Mathias Helsen <mathias@localhost.localdomain>
2014-05-24 18:04:11 +0200

    added software for the interface

---
+++ Project/curveTracer2/software/io.h
@@ -0,0 +1,28 @@
+#ifndef _IO_H
+#define _IO_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <strings.h>
+#include <assert.h>
+#include <string.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
+#include <stdarg.h>
+#include "errors.h"
+
+int myReadfull( int fd, void *buffer, int nBytes );
+
+int myRead( int fd, void *buffer, int nBytes );
+
+int myWrite( int fd, const char *format, ...);
+
+int initSerial( int *fd, int baudrate, char *devname );
+
+#endif
---
+++ Project/curveTracer2/software/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "io.h"
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
